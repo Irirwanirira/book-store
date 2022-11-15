f48c4e2eb6bc82654f109365e8f5ef35ded36084
Irirwanirira <irijoseph41@gmail.com>
2022-11-15 13:36:49 +0200

    Set linters

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,7 @@
 /* eslint-disable import/no-unresolved */
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
-import BooksReducers from './books/Books.js';
-import CategoryReducers from './Categories/categories.js';
+import BooksReducers from './books/Books';
+import CategoryReducers from './Categories/categories';
 
 const reducers = combineReducers({
   Books: BooksReducers,
