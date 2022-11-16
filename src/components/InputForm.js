1919f793e7c1202b3960685b3b988181b81b2360
Irirwanirira <irijoseph41@gmail.com>
2022-11-16 13:19:53 +0200

    Modified components

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -1,16 +1,43 @@
-/* eslint-disable */ 
-import { Component } from "react";
+/* eslint-disable */
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../redux/books/Books';
 
 const InputBooks = () => {
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+
+  const dispatch = useDispatch();
+
+  const createNewBook = (e) => {
+    e.preventDefault() 
+    const newBook ={
+      title,
+      author,
+      id: uuidv4()
+    }
+    dispatch(addBook(newBook))
+    setTitle("")
+    setAuthor("")
+  }
+
+  const titleInput = (e) => {
+    setTitle(e.target.value)
+  }
+
+  const authorInput=(e)=> {
+    setAuthor(e.target.value)
+  }
+
   return (
     <div>
-      <form>
-        <input type="text" name="author" placeholder="author" /><br />
-        <input type="text" name="book" placeholder="Title" /><br />
-        <button type="submit">Submit</button> <br />
+      <form onSubmit ={createNewBook}>
+      <input type="text" name="book" placeholder="Book" value={title} onChange={titleInput}/><br/>
+        <input type="text" name="author" placeholder="Written By" value={author} onChange={authorInput}/><br/>
+        <button type="submit">Add Book</button> <br/>
       </form>
     </div>
   );
 };
-
 export default InputBooks;
