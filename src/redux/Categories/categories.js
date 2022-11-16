100a0f3e3db2a0b4677d0dd4127647c71345c87c
Irirwanirira <irijoseph41@gmail.com>
2022-11-16 13:22:11 +0200

    Modified components

--- src/redux/Categories/categories.js
+++ src/redux/Categories/categories.js
@@ -1,21 +1,21 @@
-const CHECK_STATE = 'book-store/Categories/CHECK_STATE';
+const CHECK_STATUS = "book-store/Categories/CHECK_STATE";
 
 const CATEGORIES = [];
 
 // Action part
 
-const category = () => ({
-  type: CHECK_STATE,
+export const category = () => ({
+  type: CHECK_STATUS,
 });
 
 // Reducers
 
-const CategoryReducers = (state = CATEGORIES, action) => {
-  switch (action.type) {
-    case CHECK_STATE:
-      return 'Underconstruction';
-    default: return state;
+export const CategoryReducers = (state = CATEGORIES, action) => {
+  switch (action.type){
+    case CHECK_STATUS:
+      return "Under construction";
+    default:
+      return state;
   }
 };
-export { category };
 export default CategoryReducers;
