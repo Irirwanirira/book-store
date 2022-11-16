4a340ed33970b433eb217328e54ec0bec72fc515
Irirwanirira <irijoseph41@gmail.com>
2022-11-16 13:20:38 +0200

    Modified components

--- src/components/BookDisplay.js
+++ src/components/BookDisplay.js
@@ -1,25 +1,18 @@
-import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import Book from './Book';
 import InputBooks from './InputForm';
 /* eslint-disable */ 
 const BookDisplay = () => {
-  const [ books ] = useState([
-    { Book: 'Last Enemy', author: 'joseph', id: 1 },
-    { Book: 'wine and life', author: 'Mark', id: 2 },
-    { Book: 'night to forget', author: 'Irirwanirira', id: 3 },
-    { Book: 'recursed tannel', author: 'Actrox', id: 4 },
-  ]);
+  const Books = useSelector((state) => state.Books)
   return (
     <div>
-      {books.map((book) => (
+      {Books.map((book) => (
         <div key={book.id} className="bookCard">
-          <Book book={book} />
-          <button type="button"> Delete</button>
+          <Book book={ book } title={book.Book} author={book.author} id={book.id} />
         </div>
       ))}
       <InputBooks />
     </div>
   );
 };
-
 export default BookDisplay;
