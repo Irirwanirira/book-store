f167622ace44b6614adc8e6a301eb137f3620bcb
Irirwanirira <irijoseph41@gmail.com>
2022-11-17 22:31:13 +0200

    Added category as option and new functions

--- src/components/InputForm.js
+++ src/components/InputForm.js
@@ -5,38 +5,56 @@ import { useDispatch } from 'react-redux';
 import { addBook } from '../redux/books/books';
 
 const InputBooks = () => {
-  const [Book, setBook] = useState('');
+  const [title, settitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('') 
+  
 
   const dispatch = useDispatch();
 
   const createNewBook = (e) => {
     e.preventDefault() 
+
     const newBook ={
-      Book: Book,
-      author: author,
-      id: uuidv4()
+      item_id: uuidv4(),
+      title,
+      author,
+      category,
     }
-    
+
     dispatch(addBook(newBook))
-    setBook("");
+    settitle("");
     setAuthor("");
   }
 
   const BookInput = (e) => {
-    setBook(e.target.value)
+    settitle(e.target.value)
   }
 
   const authorInput=(e)=> {
     setAuthor(e.target.value)
   }
 
+  const anyName = (e)=> {
+    setCategory(e.target.value)
+  }
+
   return (
     <div>
       <form onSubmit ={createNewBook}>
-      <input type="text" name="Book" placeholder="Book" required value={Book} onChange={BookInput}/><br/>
+      <input type="text" name="Book" placeholder="Book" required value={title} onChange={BookInput}/><br/>
         <input type="text" name="author" placeholder="Written By" required value={author} onChange={authorInput}/><br/>
-        <button type="submit">Add Book</button> <br/>
+        <select name="category" id="category" onChange={anyName}>
+          <option>Action</option>
+          <option>Romance</option>
+          <option>Commedy</option>
+          <option>Adventure</option>
+          <option> Horror</option>
+          <option>Crime</option>
+          <option>Thriller</option>
+          <option>Drama</option>
+        </select>
+        <button type="submit">Add Book</button><br/>
       </form>
     </div>
   );
