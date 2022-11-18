7397640227aadfdb6f6e9621e19cae14a3867de7
Irirwanirira <irijoseph41@gmail.com>
2022-11-18 14:20:00 +0200

    Styled the project

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,16 +1,27 @@
 import { Link } from 'react-router-dom';
 import React from 'react';
+import { FaUserAlt } from 'react-icons/fa';
+import './NavBar.css';
 
 const NavBar = () => (
-  <div className="nav">
-    <div className="left">
-      <h1> Bookstore </h1>
-      <div className="link">
-        <Link to="/">Book</Link>
-        <Link to="/Category">Category</Link>
+  <header className="nav">
+    <div className="T-link">
+      <h1 className='title-h'> Bookstore CMS</h1>
+      <div className="link-d">
+        <Link className='link' to="/">BOOK</Link>
+        <Link className='link' to="/Category">CATEGORIES</Link>
       </div>
     </div>
-  </div>
+    <span className="userProfile-container d-flex">
+        <FaUserAlt style={{
+          color: '#0290ff',
+          height: '15px',
+          cursor: 'pointer',
+          transition: 'all ease-in 300ms',
+        }}
+        />
+      </span>
+  </header>
 );
 
 export default NavBar;
